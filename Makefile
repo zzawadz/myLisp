b5c6ae5959f3541facaee65c850b052b9a9bebb2
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-01-27 10:27:46 +0100

    Simple support for readline

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 CC = gcc
 
+ifeq ($(OS),Windows_NT)
+	READLINE = 
+else
+	READLINE = -ledit
+endif
+
+
 all: myLisp
 
 myLisp: main.c
-	$(CC) -std=c99 -Wall main.c -o myLisp
+	$(CC) $(READLINE) -std=c99 -Wall main.c -o myLisp
 
--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt)
+{
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+void add_history(char* unused){}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main (int argc, char** argv)
 {
-	puts("Lispy Version 0.0.0.0.1");
+	puts("Lispy Version 0.0.0.0.2");
 	puts("Press Ctrl + c to Exit\n");
 	
 	
 	while(1)
 	{
-		fputs("lispy>", stdout);
+		char* input = readline("lispy> ");
 		
-		fgets(input, 2048, stdin);
+		add_history(input);
 		
-		printf("No you are a %s", input);
+		printf("No you are a %s\n", input);
+		
+		free(input);
 	}
 
 	return 0;    
